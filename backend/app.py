c04ef30344af09d13268e95d41de28cca5ac0d60
Tanya <tanyakemkar@gmail.com>
2024-09-20 00:53:17 +0530

    frontend

--- backend/app.py
+++ backend/app.py
@@ -1,10 +1,20 @@
 from flask import Flask
+from flask import request
 
 app = Flask(__name__)
+
 
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
 
-if __name__ == '__main__':
+
+@app.route("/api/v1/recipes")
+def get_recipes():
+    values = request.args.get("values")
+
+    return {"values": values}
+
+
+if __name__ == "__main__":
     app.run(port=8000)
